8cbdc2a9358f39175a307bbf9a2f6170b95c82dd
WilliamBendtsen <143700870+WilliamBendtsen@users.noreply.github.com>
2024-04-19 10:30:15 +0200

    Add files via upload

--- steno/js/script.js
+++ steno/js/script.js
@@ -1,48 +1,91 @@
 window.onload = function() {
             let videosContainer = document.getElementsByClassName("videos")[0];
             let menu = document.getElementsByClassName("menu")[0];
-            let menuOptions2 = menu.getElementsByClassName("options-2")[0];
 
-            let playButton = document.getElementsByClassName("play")[0];
+            let playButton = document.getElementsByClassName("play-button")[0];
             playButton.onclick = function() {
                 playButton.style.display = "none";
-                playInitialMenu();
+                playInitialScene();
             }
 
-            let firstLink = document.getElementById("option-1-link");
+            /* Så man kan klikke på vinduet for at komme videre */
+            function windowClick() {
+                playRoofScene();
+            }
+
+            let window = document.querySelector(".window");
+            window.addEventListener("click", function() {
+                windowClick();
+            })
+
+
+            /* Så man kan klikke på månen for at komme videre */
+            let firstLink = document.getElementById("moon-box");
             firstLink.onclick = function() {
-                playClosingMenu = false;
-                showVideos(0, videosContainer.getElementsByClassName("option-1"));
+                showVideos(0, videosContainer.getElementsByClassName("moon-option"));
+                playMoonChoice();
             }
 
-            let secondLink = document.getElementById("option-2-link");
+            /* Så man kan klikke på pilen til højre for at komme videre */
+            let secondLink = document.getElementById("arrow-2");
             secondLink.onclick = function() {
-                playClosingMenu = false;
-                showVideos(0, videosContainer.getElementsByClassName("option-2"));
+                showVideos(0, videosContainer.getElementsByClassName("planets-option"));
+                playPlanetsChoice();
             }
 
-            function playInitialMenu() {
-                playVideo(videosContainer.getElementsByClassName("initial")[0], loop=false);
+
+            /* Funktioner for at man kan trykke på et element for at spille en ny scene */
+
+            /* Den første scene på værelset, hvor man skal trykke på vinduet for at komme videre */
+            function playInitialScene() {
+                playVideo(videosContainer.getElementsByClassName("room-scene")[0], loop=false);
 
                 setTimeout (function () {
                     menu.style.display = "block";
-                    menu.getElementsByClassName("initial")[0].style.display = "block";
-                    menuOptions2.style.display = "block";
-                }, 22000);
+                    menu.getElementsByClassName("window")[0].style.display = "block";
+                }, 14000);
             };
 
-            function playClosingMenu() {
-                playVideo(videosContainer.getElementsByClassName("closing")[0], loop=false);
+            /* Scenen på taget der ender med valget mellem at tage til månen eller videre i rummet */
+            function playRoofScene() {
+                playVideo(videosContainer.getElementsByClassName("roof-scene")[0],
+                loop=false);
 
-                menu.getElementsByClassName("initial")[0].style.display = "none";
+                let windowDiv = menu.getElementsByClassName("window")[0];
+                windowDiv.parentNode.removeChild(windowDiv);
 
                 setTimeout (function () {
                     menu.style.display = "block";
-                    menu.getElementsByClassName("closing")[0].style.display = "block";
-                    menuOptions2.style.display = "none";
+                    menu.getElementsByClassName("options-2")[0].style.display = "block";
+                }, 30000);
+            };
+
+            /* Scenen hvor Alex flyver hen til månen */
+            function playMoonChoice() {
+                playVideo(videosContainer.getElementsByClassName("moon-option")[0],
+                loop=false);
+
+                setTimeout (function () {
+                    menu.style.display = "block";
                 }, 1000);
             };
 
+            /* Scenen hvor Alex flyver videre til de andre planeter*/
+            function playPlanetsChoice() {
+                playVideo(videosContainer.getElementsByClassName("planets-option")[0],
+                loop=false);
+
+                let windowDiv = menu.getElementsByClassName("window")[0];
+                windowDiv.parentNode.removeChild(windowDiv);
+
+                setTimeout (function () {
+                    menu.style.display = "block";
+                }, 1000);
+            };
+
+
+
+            /* ? */
             function playVideo(videoContainer, loop=false) {
                 let lastVideoContainer = videosContainer.getElementsByClassName("active")[0];
                 lastVideoContainer.classList.remove("active");
@@ -60,6 +103,7 @@ window.onload = function() {
                 return video;
             }
 
+            /* ? */
             function showVideos(index, videos) {
                 if (index < (videos.length - 1)) {
                     hasNextVideo = true;
@@ -85,7 +129,7 @@ window.onload = function() {
                     if (hasNextVideo) {
                         showVideos(index+1, videos);
                     } else {
-                        playClosingMenu();
+                    playClosingMenu();
                     }
                 }
             }
